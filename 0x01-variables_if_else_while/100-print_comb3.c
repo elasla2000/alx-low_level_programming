e08a4eca5b20e82cbf5c5c3231372e1b19b24b57
elasla2000 <faycalelasla@gmail.com>
2023-10-07 10:14:13 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,7 +6,7 @@
 /**
  * main - Entry point
  * descripton: Prints the last digit of a randomly generated number
- * and whether it is greater than 5, less than 6, or 0.
+ * and whether it is greater than 5, less than 6.
  * Return: Always 0.
 */
 int main(void)
