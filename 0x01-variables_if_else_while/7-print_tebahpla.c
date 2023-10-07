3b75caec2bfea2297139a6c4c586b911650417ec
elasla2000 <faycalelasla@gmail.com>
2023-10-07 08:50:28 -0700

    7-print_tebahpla

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,20 +1,15 @@
 #include<stdio.h>
+
 /**
  * main - This is the main function
- * 
+ *
  * Return: Always 0
 */
 int main(void)
-{
-char ch = 'z'; /* Start with 'z', the last lowercase alphabet */
-    
+char ch = 'z'; /* Start with 'z', the last lowercase alphabet */    
 while (ch >= 'a') /* Loop from 'z' down to 'a' */
-{
 putchar(ch); /* Print the current character */
-ch--;        /* Move to the previous character in reverse order */
-}
-    
+ch--;        /* Move to the previous character in reverse order */    
 putchar('\n'); /* Print a newline character at the end */
-    
 return (0);
 }
