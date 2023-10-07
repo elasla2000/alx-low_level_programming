70c38c0dee64e0da13d5d18a11b60968e61c8615
elasla2000 <faycalelasla@gmail.com>
2023-10-07 09:42:19 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,10 +1,8 @@
 #include<stdio.h>
 /*
  * main - Entry point
- *
  * Description: This program prints the lowercase alphabet in reverse,
  *              from 'z' to 'a', followed by a newline character.
- *
  * Return: Always 0 (Success)
 */
 int main(void)
